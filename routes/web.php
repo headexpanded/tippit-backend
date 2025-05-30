e967a3d5f32f22c2456b7f7e40ddbd7dad716fed
Mark Hatherly <mhatherly@mixvoip.com>
2025-05-30 09:23:51 +0200

    Set up Sanctum for SPA authentication with API prefix.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,3 +12,6 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+*/
+
+Route::get("/sanctum/csrf-cookie", [CsrfCookieController::class, "show"]);
